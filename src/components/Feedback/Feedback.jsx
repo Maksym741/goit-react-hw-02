307a11cd585562a183f7db344a423320e2523b28
Maksym741 <pastushinamaksim@gmail.com>
2024-06-21 21:26:25 +0200

    finish

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,15 @@
+import css from "./Feedback.module.css"
+export default function Feedback({feedback, totalFeedback, positiveFeedback})
+{
+    return(
+        <div className={css.info}>
+            <ul>
+                <li>Good: {feedback.good}</li>
+                <li>Neutral: {feedback.neutral}</li>
+                <li>Bad: {feedback.bad}</li>
+                <li>Total: {totalFeedback}</li>
+                <li>Positive: {positiveFeedback}%</li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,36 @@
+import css from "./Options.module.css"
+export default function Options({updateFeedback, totalFeedback, resetFeedback}){
+    
+    return(
+        <div className={css.container}>
+            <button
+            onClick={() => updateFeedback("good")} 
+            className={css.btn}
+            >Good
+            </button>
+
+            <button
+            onClick={() => updateFeedback("neutral")} 
+            className={css.btn}
+            >Neutral
+            </button>
+
+            <button
+            onClick={() => updateFeedback("bad")} 
+            className={css.btn}
+            >Bad
+            </button>
+
+            {totalFeedback > 0 && (
+            <button 
+            onClick={resetFeedback}
+            className={css.btn}
+            >Reset
+            </button>
+            )}
+        </div>
+    )
+}
+
+
+
